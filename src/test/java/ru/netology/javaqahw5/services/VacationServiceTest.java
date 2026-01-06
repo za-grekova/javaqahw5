9f66632f704606aba04796853916a6a0d69f74d2
zagrekova <alenazagrekova@gmail.com>
2026-01-06 04:14:25 +0100

    Added ParameterizedTest

--- src/test/java/ru/netology/javaqahw5/services/VacationServiceTest.java
+++ src/test/java/ru/netology/javaqahw5/services/VacationServiceTest.java
@@ -1,26 +1,26 @@
 package ru.netology.javaqahw5.services;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
 
-    @Test
-public void thresholdOver20000() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/vacation.csv")
+public void testChangingAmounts(int expected, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
-
-        int expected = 3;
-        int actual = service.calcVacation(10_000,3000,20_000);
+        int actual = service.calcVacation(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void thresholdOver150000() {
-        VacationService service = new VacationService();
-
-        int expected = 2;
-        int actual = service.calcVacation(100_000,60_000,150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void thresholdOver150000() {
+//        VacationService service = new VacationService();
+//
+//        int expected = 2;
+//        int actual = service.calcVacation(100_000,60_000,150_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
